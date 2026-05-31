6c4c1e18b677601e0de689919d1f5805830e53b6
atuljha-tech <atulcoder27@gmail.com>
2026-05-31 15:12:59 +0530

    chore: remove remaining debug logging

--- components/dashboard/compoents/DataIngestionSection.tsx
+++ components/dashboard/compoents/DataIngestionSection.tsx
@@ -11,7 +11,7 @@ export function DataIngestionSection({ token }: { token: string }) {
         // Safe check for previous analysis
         api.get("/ingestion/last-analysis")
             .then((res) => setAnalysis(res.data))
-            .catch(() => console.log("New user state: No analysis found."));
+            .catch(() => setAnalysis(null));
     }, [token]);
 
     return (
